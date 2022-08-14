c4a19e44e847b4f23a5aebba646a58d669ff4cfd
SatouTappei <stutpi.signup@gmail.com>
2022-08-15 01:02:34 +0900

    HP減少時の演出を作成

--- Assets/PlayerUIManager.cs
+++ Assets/PlayerUIManager.cs
@@ -11,6 +11,11 @@ public class PlayerUIManager : MonoBehaviour
 {
     /// <summary>経過ターンを表示するテキスト</summary>
     [SerializeField] Text _dispTurnText;
+    /// <summary>ライフポイントを表すアイコンの親</summary>
+    [SerializeField] Transform _lifePointItem;
+
+    // DotWeenの挙動を確認するためだけの変数、確認が終わったら消す
+    int _testLifePoint = 3;
 
     void Start()
     {
@@ -19,7 +24,11 @@ public class PlayerUIManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _testLifePoint--;
+            DecreaseLifePoint(_testLifePoint);
+        }
     }
 
     /// <summary>経過したターンを表示する</summary>
@@ -30,6 +39,16 @@ public class PlayerUIManager : MonoBehaviour
         sequence.Join(_dispTurnText.transform.DOScale(_dispTurnText.transform.localScale * 1.2f, 0.15f));
         sequence.Append(_dispTurnText.transform.DOScale(_dispTurnText.transform.localScale, 0.15f));
         sequence.Play();
-        // TODO:DotWeenを使用したアニメーションを使いたい
+    }
+
+    /// <summary>ライフを減少させる</summary>
+    public void DecreaseLifePoint(int life)
+    {
+        Transform child = _lifePointItem.GetChild(life);
+        Sequence sequence = DOTween.Sequence();
+        sequence.Join(child.DOMoveY(child.position.y - 100.0f, 1.0f).SetEase(Ease.OutBounce));
+        sequence.Join(child.DOMoveX(child.position.x + 25.0f, 1.0f));
+        sequence.Join(child.GetComponent<Image>().DOFade(0, 1.0f));
+        sequence.Play();
     }
 }
