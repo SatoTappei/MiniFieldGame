a8ff8d5c6c03699bc8ed8de7efa010ec4c6ed654
SatouTappei <stutpi.signup@gmail.com>
2022-09-24 17:22:18 +0900

    瀕死エフェクトの表示順を変更

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -79,8 +79,6 @@ public class MapManager : MonoBehaviour
     [SerializeField] Transform _coinParent;
     /// <summary>タイルを隠す雲</summary>
     [SerializeField] GameObject _cloud;
-    /// <summary>デバッグ用にRキーでシーンの再読み込みをさせるかどうか</summary>
-    [SerializeField] bool isDebug;
     /// <summary>フロアに雲を生成するか</summary>
     bool _isCloudy;
     /// <summary>フロアに生成する生成する敵</summary>
@@ -116,11 +114,13 @@ public class MapManager : MonoBehaviour
 
     void Update()
     {
-        // マップをリロードするテスト
-        if (Input.GetKeyDown(KeyCode.R) && isDebug)
+#if UNITY_EDITOR
+        // マップをリロードする
+        if (Input.GetKeyDown(KeyCode.R))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
+#endif
     }
 
     /// <summary>ステージの初期化、開始時にPlayerSceneManagerから呼ばれる</summary>
